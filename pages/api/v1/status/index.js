718a4cd7eeafa1b6bb456586077ab5f14880bb23
Victor Barroso Lino <victorbarrosolino@gmail.com>
2024-10-01 12:52:43 -0300

    Adiciona `database.js` e o arquivo `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,11 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1+1;");
+  console.log(result);
   response.status(200).json({
-    chave: "São acima da média"
-  })
+    chave: "São acima da média",
+  });
 }
 
 export default status;
