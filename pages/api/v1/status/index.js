53a6d6f1882ef98523a558adad19f3bb0d13e305
Victor Barroso Lino <victorbarrosolino@gmail.com>
2024-10-02 05:29:21 -0300

    `/api/v1/status` endpoint finalizado

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,24 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1;");
-  console.log(result);
+  const databaseName = process.env.POSTGRES_DB;
+  const updateAt = new Date().toISOString();
+  const databaseVersion = await database.query("SHOW server_version;");
+  const maxConections = await database.query("SHOW max_connections;");
+  const openedConnections = await database.query({
+    text: "SELECT count(*)::int from pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
   response.status(200).json({
-    chave: "São acima da média",
+    update_at: updateAt,
+    dependecies: {
+      database: {
+        postgres_version: databaseVersion.rows[0].server_version,
+        max_connections: parseInt(maxConections.rows[0].max_connections),
+        opened_connections: parseInt(openedConnections.rows[0].count),
+      },
+    },
   });
 }
 
