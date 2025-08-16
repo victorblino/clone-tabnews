d955748c6db4ff21d0b43dc6ffe4f1dd004ac115
Victor Barroso Lino <victorbarrosolino@gmail.com>
2025-08-16 12:18:17 -0300

    test fix orchestrator dont working in tests action

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,7 +6,7 @@ async function waitForAllServices() {
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
       retries: 100,
-      maxTimeout: 5000,
+      maxTimeout: 1000,
     });
 
     async function fetchStatusPage() {
