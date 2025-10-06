5708860f636f6e9837d4331bb195f16e5b1d413d
Victor Barroso Lino <victorbarrosolino@gmail.com>
2025-10-06 17:12:23 -0300

    feat(status): add initial status page

--- pages/status/index.js
+++ pages/status/index.js
@@ -14,19 +14,43 @@ function UpdateAt() {
   let updateAtText = "Carregando...";
   if (!isLoading && data) {
     console.log(data.update_at);
-    
-    updateAtText = new Date(data.update_at).toLocaleString("pt-BR")
+
+    updateAtText = new Date(data.update_at).toLocaleString("pt-BR");
   }
   return updateAtText;
 }
 
+function DatabaseStatus() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchStatus, {
+    refreshInterval: 2000,
+  });
+
+  console.log(data);
+  
+
+  let databaseStatusText = "Carregando...";
+  if (!isLoading && data) {
+    databaseStatusText = (
+      <>
+        <div>
+          <p>Versão do Postgres: {data.dependecies.database.postgres_version}</p>
+          <p>Máximo de conexões: {data.dependecies.database.max_connections}</p>
+          <p>Conexões abertas na DB: {data.dependecies.database.opened_connections}</p>
+        </div>
+      </>
+    );
+  }
+  return databaseStatusText;
+}
 
 // todo: página com todas as informações do /api/v1/status
 export default function StatusPage() {
   return (
     <>
       <h1>Status</h1>
-      <UpdateAt/>
+      <UpdateAt />
+      <h2>Database:</h2>
+      <DatabaseStatus />
     </>
   );
 }
